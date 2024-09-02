ab88643b3c6efe16de0482f404d4cf05e1e322e9
minthug <jkisimmortal4@gmail.com>
2024-09-02 15:57:43 +0900

    Add new query methods and adjust filter order

--- demo 2/src/main/java/com/example/demo/config/security/securityConfig.java
+++ demo 2/src/main/java/com/example/demo/config/security/securityConfig.java
@@ -23,6 +23,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -61,7 +62,7 @@ public class securityConfig {
                         .userInfoEndpoint(userInfo -> userInfo.userService(customOauthUserService))
                 );
 
-        http.addFilterAfter(customJsonUsernamePasswordAuthenticationFilter());
+        http.addFilterAfter(customJsonUsernamePasswordAuthenticationFilter(), LogoutFilter.class);
         http.addFilterBefore(jwtAuthenticationProcessingFilter(), CustomJsonUsernamePasswordAuthenticationFilter.class);
 
         return http.build();
--- demo 2/src/main/java/com/example/demo/repository/CommentRepository.java
+++ demo 2/src/main/java/com/example/demo/repository/CommentRepository.java
@@ -2,6 +2,28 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import javax.swing.text.html.Option;
+import java.awt.print.Pageable;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    List<Comment> findByDiaryId(Long diaryId);
+
+    List<Comment> findByDiaryId(Long diaryId, Pageable pageable);
+
+    List<Comment> findByMemberId(Long memberId);
+
+    List<Comment> findByCreatedAtBetween(LocalDateTime start, LocalDateTime end);
+
+    @Query("select count (C) from Comment c where c.diary.id = :diaryId")
+    long countByDiaryId(@Param("diaryId") Long diaryId);
+
+    @Query("select c from Comment c Join fetch c.diary d join fetch c.member m where c.id = :id")
+    Optional<Comment> findByIdWithDiaryAndMember(@Param("id") Long id);
 }
